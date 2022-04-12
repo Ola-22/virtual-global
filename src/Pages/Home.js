8602d209f998b5575ad1b37384785822357624f5
Ola-22 <olanazly@gmail.com>
2022-04-12 14:44:23 +0300

    Add Nav, Header components of the home page

---
+++ src/Pages/Home.js
@@ -0,0 +1,11 @@
+import Header from "../Components/Header";
+import Nav from "../Components/Nav";
+
+export default function Home() {
+  return (
+    <div style={{ width: "100%" }}>
+      <Nav />
+      <Header />
+    </div>
+  );
+}
