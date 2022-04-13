95c35696a87b49f6d7258a160702a24e57ee0b72
Ola-22 <olanazly@gmail.com>
2022-04-13 22:27:26 +0300

    Add banner section and social component

---
+++ src/Components/Banner/index.js
@@ -0,0 +1,33 @@
+import Social from "../Social";
+import * as S from "./style";
+
+function Banner() {
+  return (
+    <S.BannerContainer>
+      <div>
+        <h4>NUMBER of VIRTUAL Citizens</h4>
+        <h2>1,115,887</h2>
+      </div>
+      <div>
+        <p>
+          Do you suffer when witnessing killings and massacres due to wars? Do
+          you feel the danger of
+        </p>
+
+        <S.BackGround>
+          <span></span>
+          <img
+            src="/images/banner.png"
+            alt="banner virtual global"
+            // width="419"
+            // height="410"
+          />
+        </S.BackGround>
+
+        <Social />
+      </div>
+    </S.BannerContainer>
+  );
+}
+
+export default Banner;
