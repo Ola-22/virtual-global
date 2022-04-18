971a2ed30c064bee3d2bede7bcc5f54ca347c503
Ola-22 <olanazly@gmail.com>
2022-04-18 14:44:56 +0300

    create accordion, useState to setShow accordion by index

---
+++ src/Components/Answer&Questions/Accordion.js
@@ -0,0 +1,60 @@
+import { useState } from "react";
+
+const data = [
+  {
+    questions: "Question Title",
+    answer:
+      "In light of the current situation of wars and human tragedies in the World and the absence of a promising horizon where real peace and constructive cooperation between people prevails, this proposal urges us to think outside the box, looking for creative solutions to try to change this reality. The World is made up of many nations and entities that differ in interests and goals. Therefore, it is difficult to build a unified strategy for a better future for human life on Earth! To overcome this reality, this ambitious – long term- proposal ",
+  },
+  {
+    questions: "Question Title",
+    answer:
+      "In light of the current situation of wars and human tragedies in the World and the absence of a promising horizon where real peace and constructive cooperation between people prevails, this proposal urges us to think outside the box, looking for creative solutions to try to change this reality. The World is made up of many nations and entities that differ in interests and goals. Therefore, it is difficult to build a unified strategy for a better future for human life on Earth! To overcome this reality, this ambitious – long term- proposal ",
+  },
+  {
+    questions: "Question Title",
+    answer:
+      "In light of the current situation of wars and human tragedies in the World and the absence of a promising horizon where real peace and constructive cooperation between people prevails, this proposal urges us to think outside the box, looking for creative solutions to try to change this reality. The World is made up of many nations and entities that differ in interests and goals. Therefore, it is difficult to build a unified strategy for a better future for human life on Earth! To overcome this reality, this ambitious – long term- proposal ",
+  },
+  {
+    questions: "Question Title",
+    answer:
+      "In light of the current situation of wars and human tragedies in the World and the absence of a promising horizon where real peace and constructive cooperation between people prevails, this proposal urges us to think outside the box, looking for creative solutions to try to change this reality. The World is made up of many nations and entities that differ in interests and goals. Therefore, it is difficult to build a unified strategy for a better future for human life on Earth! To overcome this reality, this ambitious – long term- proposal ",
+  },
+];
+
+function Accordion() {
+  const [show, setShow] = useState(-1);
+
+  function handleToggle(index) {
+    if (index === show) {
+      setShow(-1);
+      return;
+    }
+
+    setShow(index);
+  }
+
+  return (
+    <div>
+      {data.map((item, index) => (
+        <div key={index} onClick={() => handleToggle(index)}>
+          <div className="accordion_faq">
+            <h3 className={show === index ? "active" : ""}>{item.questions}</h3>
+          </div>
+          <div>
+            <img src="/images/Accordion.png" alt="Accordion" />
+          </div>
+
+          <div>
+            <p className={show === index ? "active" : "inActive"}>
+              {item.answer}
+            </p>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default Accordion;
