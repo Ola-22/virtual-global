f9ce202b8a08fa406d1e2fabe65461aa818a9e08
Ola-22 <olanazly@gmail.com>
2022-04-18 14:46:25 +0300

    use react slick library to slide the card

--- src/Components/PopularDiscussions/index.js
+++ src/Components/PopularDiscussions/index.js
@@ -1,7 +1,42 @@
+import Slider from "react-slick";
+import CardPolular from "./CardPopular";
 import * as S from "./style";
 
-function PopularDiscussions() {
-  return <S.PopularContainer>style</S.PopularContainer>;
+function SampleNextArrow(props) {
+  const { className, style, onClick } = props;
+  return (
+    <S.NextArrow className={className} onClick={onClick}>
+      <img src="/images/arrowNext.png" />
+    </S.NextArrow>
+  );
 }
 
-export default PopularDiscussions;
+function SamplePrevArrow(props) {
+  const { className, style, onClick } = props;
+  return (
+    <S.PrevArrow className={className} onClick={onClick}>
+      <img src="/images/arrowPrev.png" />
+    </S.PrevArrow>
+  );
+}
+
+export default function PopularDiscussions() {
+  const settings = {
+    dots: false,
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    nextArrow: <SampleNextArrow />,
+    prevArrow: <SamplePrevArrow />,
+  };
+  return (
+    <S.PopularContainer>
+      <h1>popular Discussions</h1>
+      <Slider {...settings}>
+        <CardPolular />
+        <CardPolular />
+        <CardPolular />
+      </Slider>
+    </S.PopularContainer>
+  );
+}
