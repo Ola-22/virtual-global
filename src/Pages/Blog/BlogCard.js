e3f3d734183ce2389a63e132c866b04869d91bbb
Ola-22 <olanazly@gmail.com>
2022-06-30 14:06:25 +0300

    Edit auth of the blog page

--- src/Pages/Blog/BlogCard.js
+++ src/Pages/Blog/BlogCard.js
@@ -1,8 +1,19 @@
 import * as S from "./style";
 import Button from "../../Components/Button";
 import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
+import authService from "../Register/Auth";
 
 function BlogCard({ title, date, content, image, author, id }) {
+  const [currentUser, setCurrentUser] = useState(undefined);
+
+  useEffect(() => {
+    const user = authService.getCurrentUser();
+
+    if (user) {
+      setCurrentUser(user);
+    }
+  }, []);
   return (
     <S.BlogCard>
       <div className="card-body">
@@ -17,7 +28,8 @@ function BlogCard({ title, date, content, image, author, id }) {
           </div>
         </div>
         <p>{content}</p>
-        <Link to={`/blog/${id}`}>
+
+        <Link to={`${!currentUser ? "/login" : `/blog/${id}`}`}>
           <Button title="Read more" />
         </Link>
       </div>
